2350af1cbf03a425b9bc610fe12a1b3689d41e73
aea <aea@debian-aea.aea>
2023-01-20 16:48:44 +0100

    création dossier packets si non existant

--- lib/packet.c
+++ lib/packet.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "packet.h"
 
 FILE* createPacket(char* packetName)
 {
+    struct stat s = {0};
     char* directory = "packets/";
     char* extension = ".json";
     size_t len = strlen(directory) + strlen(packetName) + strlen(extension);
@@ -14,6 +16,10 @@ FILE* createPacket(char* packetName)
     strcpy(filePath, directory);
     strcat(filePath, packetName);
     strcat(filePath, extension);
+
+    if (stat(directory, &s) == -1) {
+        mkdir(directory, 0777);
+    }
 
     FILE* filePointer = fopen(filePath, "w");
 
