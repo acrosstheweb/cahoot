8949d1c5f34c7c5d3436d66e3c4b366eaf4f45a2
aea <aea@debian-aea.aea>
2023-02-05 01:22:08 +0100

    hosting page

---
+++ includes/hosting.h
@@ -0,0 +1,8 @@
+//#warning createPacket
+#ifndef _HOSTING
+#define _HOSTING
+
+#include "struct.h"
+
+int hosting(Window* window);
+#endif
---
+++ vue/hosting.c
@@ -0,0 +1,89 @@
+// TODO: cleanup on leave function to free all resources in array
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "../includes/hosting.h"
+#include "../includes/functionsDisplay.h"
+#include "../includes/functions.h"
+
+int hosting(Window* window) {
+
+    // Créer les textures
+    char* ip = getIp();
+    SDL_Texture* messageTexture = textureFromMessage(window->renderer, "Vous hebergez une partie sur l'adresse suivante:", setColor("Black"), window->font);
+    SDL_Texture* ipTexture = textureFromMessage(window->renderer, ip , setColor("Black"), window->font);
+
+    // States* message = setStates(messageTexture, messageTexture);
+    // States* ip = setStates(ipTexture, ipTexture);
+
+    // Définir les positions des boutons (x, y, w, h)
+    SDL_Rect messageRect = {
+        (SCREEN_WIDTH - getTextWidth("Vous hebergez une partie sur l'adresse suivante:", 50)) / 2,
+        200,
+        getTextWidth("Vous hebergez une partie sur l'adresse suivante:", 50),
+        50
+    };
+    SDL_Rect ipRect = {
+        (SCREEN_WIDTH - getTextWidth(ip, 50)) / 2,
+        300,
+        getTextWidth(ip, 50),
+        50
+    };
+
+
+
+    Node* first = NULL;
+    addTemplateToList(&first, window, 1, 0, 0, "");
+    
+    // Boucle principale
+    int quit = 0;
+    while (!quit) {
+        SDL_Event e;
+        Node* current = first;
+        int x, y;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_MOUSEMOTION:
+                    x = e.motion.x;
+                    y = e.motion.y;
+                    checkHover(first, x, y);
+                    break;
+                
+                case SDL_MOUSEBUTTONDOWN:
+                    x = e.motion.x;
+                    y = e.motion.y;
+                    checkHover(first, x, y);
+                    if (first != NULL) {
+                        do {
+                            if (current->button.isHovered && current->button.isClickable){
+                                return current->button.isClickable;
+                            }
+                            current = current->next;
+                        }while (current != first);
+                    }
+                    break;
+                
+                case SDL_QUIT:
+                    return 1;
+            }
+        }
+
+        // Effacer l'écran
+        SDL_SetRenderDrawColor(window->renderer, 0xF1, 0xFA, 0xEE, 0xFF);
+        SDL_RenderClear(window->renderer);
+
+        if (first != NULL) {
+            do {
+                display(window->renderer, current->button);
+                current = current->next;
+            }while (current != first);
+        }
+        SDL_RenderCopy(window->renderer, messageTexture, NULL, &messageRect);
+        SDL_RenderCopy(window->renderer, ipTexture, NULL, &ipRect);
+
+        // Mettre à jour l'affichage
+        SDL_RenderPresent(window->renderer);
+    }
+    return 1;
+}
